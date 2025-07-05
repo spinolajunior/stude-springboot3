89f1467b31c25f73c65bea74808d05820f9a1b5a
Roberto Jr <113848806+spinolajunior@users.noreply.github.com>
2025-07-05 01:12:20 -0300

    Atualiza versão do Java para 21 e adiciona Spring Boot Actuator; implementa métodos findAll e findById no UserService e ajusta UserResource para retornar lista de usuários.

--- src/main/java/com/robertojr/course/resouces/UserResouce.java
+++ src/main/java/com/robertojr/course/resouces/UserResouce.java
@@ -1,20 +1,38 @@
 package com.robertojr.course.resouces;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.robertojr.course.entities.User;
+import com.robertojr.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResouce {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "75983717345", "120777Ce");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		
+		List<User> list = service.findAll();
+		
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		
+		User objUser = service.findById(id);
+		
+		return ResponseEntity.ok().body(objUser);
 	}
 	
 }
---
+++ src/main/java/com/robertojr/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.robertojr.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.robertojr.course.entities.User;
+import com.robertojr.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> optional = userRepository.findById(id);
+		return optional.get();
+	}
+
+}
